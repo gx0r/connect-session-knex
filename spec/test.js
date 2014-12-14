8dedaba3ad591e6608540709601687cbdbc170a8
llambda <llambda@users.noreply.github.com>
2014-12-14 01:48:49 -0600

    Add travis ci for mysql

--- spec/test.js
+++ spec/test.js
@@ -7,16 +7,6 @@ var KnexStore = require('../lib/connect-session-knex.js')(session);
 
 var sqliteStore = new KnexStore({db: ':memory:', dir: 'dbs'});
 
-// var knexPg = require('knex')({
-// 	client: 'pg',
-// 	connection: {
-// 		host: '127.0.0.1',
-// 		user: 'session',
-// 		password: 'session',
-// 		database: 'session'
-// 	}
-// });
-
 var knexPg = require('knex')({
 	client: 'pg',
 	connection: {
@@ -27,7 +17,20 @@ var knexPg = require('knex')({
 	}
 });
 
+var knexMysql = require('knex')({
+	client: 'mysql',
+	connection: {
+		host: '127.0.0.1',
+		user: 'travis',
+		password: '',
+		database: 'travis_ci_test'
+	}
+});
+
 var postgresStore = new KnexStore({ knex: knexPg });
+
+var mysqlStore = new KnexStore({ knex: knexMysql });
+
 
 describe('connect-session-knex sqlite3 test suite', function() {
 	before(function() {
@@ -175,6 +178,86 @@ describe('connect-session-knex postgreSQL test suite', function() {
 		this.store.set('555666777', {cookie: {maxAge: 1000}, name: 'Rob Dobilina'}, function(err, rows) {
 			should.not.exist(err, 'set() returned an error');
 			rows.rowCount.should.eql(1);
+
+			that.store.destroy('555666777', function(err) {
+				should.not.exist(err, 'destroy returned an error');
+
+				that.store.length(function(err, len) {
+					should.not.exist(err, 'session count after destroy returned an error');
+					should.exist(len);
+					len.should.equal(0);
+					done();
+				});
+			});
+		});
+	});
+});
+
+
+describe('connect-session-knex MySQL test suite', function() {
+	before(function() {
+		this.store = mysqlStore;
+	});
+
+	after(function() {
+		// this.store.close();
+	});
+
+	it('it should save a new session record', function(done) {
+		this.store.set('1111222233334444', {cookie: {maxAge: 20000}, name: 'sample name'}, function(err, rows) {
+			should.not.exist(err, err);
+			rows[0].affectedRows.should.eql(1);
+			done();
+		});
+	});
+
+
+	it('it should retrieve an active session', function(done) {
+		this.store.get('1111222233334444', function(err, session) {
+			should.not.exist(err, 'get() returned an error');
+			should.exist(session);
+			(session).should.eql({cookie: {maxAge: 20000 }, name: 'sample name'});
+			done();
+		});
+	});
+
+	it('it should gracefully handle retrieving an unkonwn session', function(done) {
+		this.store.get('hope-and-change', function(err, rows) {
+			should.not.exist(err, 'get() unknown session returned an error');
+			should.equal(undefined, rows, 'unknown session is not undefined');
+			done();
+		});
+	});
+
+	it('it should only contain one session', function(done) {
+		this.store.length(function(err, len) {
+			should.not.exist(err, 'session count returned an error');
+			should.exist(len);
+			len.should.equal(1);
+			done();
+		});
+	});
+
+	it('it should clear all session records', function(done) {
+		var that = this;
+		this.store.clear(function(err, success) {
+			should.not.exist(err, 'clear returned an error');
+			success.should.be.true;
+
+			that.store.length(function(err, len) {
+				should.not.exist(err, 'session count after clear returned an error');
+				should.exist(len);
+				len.should.equal(0);
+				done();
+			});
+		});
+	});
+
+	it('it should destroy a session', function(done) {
+		var that = this;
+		this.store.set('555666777', {cookie: {maxAge: 1000}, name: 'Rob Dobilina'}, function(err, rows) {
+			should.not.exist(err, 'set() returned an error');
+			rows[0].affectedRows.should.eql(1);
 
 			that.store.destroy('555666777', function(err) {
 				should.not.exist(err, 'destroy returned an error');
