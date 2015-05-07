3b5a42e97800269e42148016e99cce5ddee85e44
llambda <xxgsoftware@gmail.com>
2015-05-07 17:28:42 -0500

    Reorganize code, that => self, switch to using tape for tests, fix SQLite variable token substitution.

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ function timestampTypeName(knex) {
 */
 function dbCleanup(store) {
 	return store.ready.then(function () {
-		store.knex(store.tablename).del().whereRaw('expired < CAST(? as ' + timestampTypeName(store.knex) + ')', nowAsISO());
+		return store.knex(store.tablename).del()
+		.whereRaw('expired < CAST(? as ' + timestampTypeName(store.knex) + ')', nowAsISO());
 	});
 }
 
@@ -46,13 +47,13 @@ function dbCleanup(store) {
 * @api public
 */
 function KnexStore(options) {
-	var that = this;
+	var self = this;
 
 	options = options || {};
-	Store.call(this, options);
+	Store.call(self, options);
 
-	this.tablename = options.tablename || 'sessions';
-	this.knex = options.knex || require('knex')({
+	self.tablename = options.tablename || 'sessions';
+	self.knex = options.knex || require('knex')({
 		client: 'sqlite3',
 		// debug: true,
 		connection: {
@@ -60,13 +61,13 @@ function KnexStore(options) {
 		}
 	});
 
-	this.ready = that.knex.schema.hasTable(that.tablename)
+	self.ready = self.knex.schema.hasTable(self.tablename)
 	.then(function (exists) {
 		if (!exists) {
-			return that.knex.schema.createTable(that.tablename, function (table) {
+			return self.knex.schema.createTable(self.tablename, function (table) {
 				table.string('sid').primary();
 				table.json('sess').notNullable();
-				if (['mysql', 'mariasql'].indexOf(that.knex.client.dialect) > -1) {
+				if (['mysql', 'mariasql'].indexOf(self.knex.client.dialect) > -1) {
 					table.dateTime('expired').notNullable();
 				} else {
 					table.timestamp('expired', 'true').notNullable();
@@ -75,8 +76,8 @@ function KnexStore(options) {
 		}
 	})
 	.then(function () {
-		dbCleanup(that);
-		setInterval(dbCleanup, oneDay, that).unref();
+		dbCleanup(self);
+		setInterval(dbCleanup, oneDay, self).unref();
 	});
 }
 
@@ -91,13 +92,13 @@ util.inherits(KnexStore, Store);
 * @api public
 */
 KnexStore.prototype.get = function(sid, fn) {
-	var that = this;
-	return that.ready.then(function () {
-		return that.knex
+	var self = this;
+	return self.ready.then(function () {
+		return self.knex
 			.select('sess')
-			.from(that.tablename)
+			.from(self.tablename)
 			.where('sid', '=', sid)
-			.andWhereRaw('CAST(? as ' + timestampTypeName(that.knex) + ') <= expired', nowAsISO())
+			.andWhereRaw('CAST(? as ' + timestampTypeName(self.knex) + ') <= expired', nowAsISO())
 			.then(function (response) {
 				if (fn) {
 					if (response[0]) {
@@ -127,7 +128,7 @@ KnexStore.prototype.get = function(sid, fn) {
 * @api public
 */
 KnexStore.prototype.set = function(sid, sess, fn) {
-	var that = this;
+	var self = this;
 	var maxAge = sess.cookie.maxAge;
 	var now = new Date().getTime();
 	var expired = maxAge ? now + maxAge : now + oneDay;
@@ -137,7 +138,7 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 					'), ' +
 					'upsert as ' +
 					'( ' +
-					'  update ' + that.tablename + ' cs set ' +
+					'  update ' + self.tablename + ' cs set ' +
 					'    sid = nv.sid, ' +
 					'    expired = nv.expired, ' +
 					'    sess = nv.sess ' +
@@ -145,19 +146,19 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 					'  where cs.sid = nv.sid ' +
 					'  returning cs.* ' +
 					')' +
-					'insert into ' + that.tablename + ' (sid, expired, sess) ' +
+					'insert into ' + self.tablename + ' (sid, expired, sess) ' +
 					'select sid, expired, sess ' +
 					'from new_values ' +
 					'where not exists (select 1 from upsert up where up.sid = new_values.sid)';
 
-	var sqlitefastq = 'insert or replace into ' + that.tablename + ' (sid, expired, sess) values ($1, $2, $3);';
+	var sqlitefastq = 'insert or replace into ' + self.tablename + ' (sid, expired, sess) values (?, ?, ?);';
 
-	var mysqlfastq = 'insert into ' + that.tablename + ' (sid, expired, sess) values (?, ?, ?) on duplicate key update expired=values(expired), sess=values(sess);';
+	var mysqlfastq = 'insert into ' + self.tablename + ' (sid, expired, sess) values (?, ?, ?) on duplicate key update expired=values(expired), sess=values(sess);';
 
-	if (that.knex.client.dialect === 'sqlite3') {
+	if (self.knex.client.dialect === 'sqlite3') {
 		// sqlite optimized query
-		return that.ready.then(function () {
-			return that.knex.raw(sqlitefastq, [sid, new Date(expired).toISOString(), sess ])
+		return self.ready.then(function () {
+			return self.knex.raw(sqlitefastq, [sid, new Date(expired).toISOString(), sess ])
 			.then(function (result) {
 				if (fn && result instanceof Array) {
 					fn(null, [1]);
@@ -168,10 +169,10 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 				throw err;
 			});
 		});
-	} else if (that.knex.client.dialect === 'postgresql') {
+	} else if (self.knex.client.dialect === 'postgresql') {
 		// postgresql optimized query
-		return that.ready.then(function () {
-			return that.knex.raw(postgresfastq, [sid, new Date(expired).toISOString(), sess ])
+		return self.ready.then(function () {
+			return self.knex.raw(postgresfastq, [sid, new Date(expired).toISOString(), sess ])
 			.then(function (result) {
 				if (fn) {
 					fn(null, result);
@@ -182,10 +183,10 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 				throw err;
 			});
 		});
-	} else if (['mysql', 'mariasql'].indexOf(that.knex.client.dialect) > -1) {
+	} else if (['mysql', 'mariasql'].indexOf(self.knex.client.dialect) > -1) {
 		// mysql/mariaDB optimized query
-		return that.ready.then(function () {
-			return that.knex.raw(mysqlfastq, [sid, new Date(expired).toISOString(), sess ])
+		return self.ready.then(function () {
+			return self.knex.raw(mysqlfastq, [sid, new Date(expired).toISOString(), sess ])
 			.then(function (result) {
 				if (fn) {
 					fn(null, result);
@@ -197,19 +198,19 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 			});
 		});
 	} else {
-		return that.ready.then(function () {
-			return that.knex.transaction(function (trx) {
-				return trx.select('*').forUpdate().from(that.tablename).where('sid', '=', sid)
+		return self.ready.then(function () {
+			return self.knex.transaction(function (trx) {
+				return trx.select('*').forUpdate().from(self.tablename).where('sid', '=', sid)
 				.then(function (foundKeys) {
 					if (foundKeys.length === 0) {
-						return trx.from(that.tablename)
+						return trx.from(self.tablename)
 						.insert({
 							sid: sid,
 							expired: new Date(expired).toISOString(),
 							sess: sess
 						});
 					} else {
-						return trx(that.tablename)
+						return trx(self.tablename)
 						.where('sid', '=', sid)
 						.update({
 							expired: new Date(expired).toISOString(),
@@ -219,13 +220,11 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 				});
 			})
 			.then(function (res) {
-				if (fn) {
-					fn(null, res);
-				}
+				if (fn) fn(null, res);
 				return res;
 			})
 			.catch(function(err) {
-				fn(err);
+			if (fn) fn(err);
 				throw err;
 			});
 		});
@@ -240,15 +239,14 @@ KnexStore.prototype.set = function(sid, sess, fn) {
 * @api public
 */
 KnexStore.prototype.destroy = function(sid, fn) {
-	var that = this;
-	return that.ready.then(function () {
-		return that.knex.del().from(that.tablename).where('sid', '=', sid).then(function () {
-			if (fn) {
-				fn(null, true);
-			}
+	var self = this;
+	return self.ready.then(function () {
+		return self.knex.del().from(self.tablename).where('sid', '=', sid).then(function (res) {
+			if (fn) fn(null, true);
+			return res;
 		})
 		.catch(function(err) {
-			fn(err);
+			if (fn) fn(err);
 			throw err;
 		});
 	});
@@ -262,9 +260,9 @@ KnexStore.prototype.destroy = function(sid, fn) {
 * @api public
 */
 KnexStore.prototype.length = function(fn) {
-	var that = this;
-	return that.ready.then(function () {
-		return that.knex.count('sid as count').from(that.tablename).then(function (response) {
+	var self = this;
+	return self.ready.then(function () {
+		return self.knex.count('sid as count').from(self.tablename).then(function (response) {
 			if (fn) {
 				fn(null, response[0].count | 0);
 			}
@@ -284,9 +282,9 @@ KnexStore.prototype.length = function(fn) {
 * @api public
 */
 KnexStore.prototype.clear = function(fn) {
-	var that = this;
-	return that.ready.then(function () {
-		return that.knex.del().from(that.tablename).then(function () {
+	var self = this;
+	return self.ready.then(function () {
+		return self.knex.del().from(self.tablename).then(function () {
 			if (fn) {
 				fn(null, true);
 			}
