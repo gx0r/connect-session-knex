33d73acff520c1ee142e974ef6d23757aece823e
Grant Miner <llambda@users.noreply.github.com>
2015-09-08 21:28:06 -0500

    tab indent

--- example-postgres.js
+++ example-postgres.js
@@ -6,35 +6,35 @@ const KnexSessionStore = require('connect-session-knex')(session);
 
 const Knex = require('knex');
 const knex = Knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '',
-    database: 'travis_ci_test'
-  }
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '',
+        database: 'travis_ci_test'
+    }
 });
 
 const store = new KnexSessionStore({
-  knex: knex,
-  tablename: 'sessions' // optional. Defaults to 'sessions'
+    knex: knex,
+    tablename: 'sessions' // optional. Defaults to 'sessions'
 });
 
 
 app.use(session({
-  secret: 'keyboard cat',
-  cookie: {
-    maxAge: 10000 // ten seconds, for testing
-  },
-  store: store
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 10000 // ten seconds, for testing
+    },
+    store: store
 }));
 
 var count = 0;
 
 app.use('/', function (req, res, next) {
-  var n = req.session.views || 0
-  req.session.views = ++n
-  res.end(n + ' views')
+    var n = req.session.views || 0
+    req.session.views = ++n
+    res.end(n + ' views')
 })
 
 app.listen(3000);
