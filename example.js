d2761aa94a1d9effb6a70cf7c8cf09a4c3251f05
Grant Miner <llambda@users.noreply.github.com>
2015-09-08 21:21:26 -0500

    In example, have intervals printing session count and clearing all sessions.

--- example.js
+++ example.js
@@ -1,24 +1,36 @@
 const express = require('express'); // Express 4
-const app = express(); 
+const app = express();
 
 const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(session);
+const KnexSessionStore = require('./index.js')(session);
 const store = new KnexSessionStore(/* options here */); // defaults to a sqlite3 database
 
 app.use(session({
-  secret: 'keyboard cat',
-  cookie: {
-    maxAge: 10000 // ten seconds, for testing
-  },
-  store: store
+    secret: 'keyboard cat',
+    cookie: {
+        maxAge: 30000 // 2 seconds for testing
+    },
+    store: store
 }));
 
 var count = 0;
 
 app.use('/', function (req, res, next) {
-  var n = req.session.views || 0
-  req.session.views = ++n
-  res.end(n + ' views')
+    var n = req.session.views || 0
+    req.session.views = ++n
+    res.end(n + ' views')
 })
 
 app.listen(3000);
+
+setInterval(function () {
+    store.length().then(function (length) {
+        console.log('There are ' + JSON.stringify(length) + ' sessions');
+    })
+}, 2000);
+
+setInterval(function () {
+    store.clear().then(function (length) {
+        console.log('Cleared ' + JSON.stringify(length) + ' sessions');
+    })
+}, 30000);
