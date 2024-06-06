aed74993090d7760f8f3c16c1ee62403bb6c2b0f
Grant Miner <gx0r@proton.me>
2024-06-05 21:15:12 -0500

    Node test runner

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,6 @@
-const test = require("tape");
+const test = require("node:test");
+const assert = require("node:assert");
+
 const session = require("express-session");
 const knexPg = require("knex")({
   client: "pg",
@@ -45,180 +47,106 @@ stores.push(
 // );
 
 stores.forEach((store) => {
-  test("initial clear", (t) => {
-    t.plan(3);
-    store.clear((err) => {
-      t.error(err);
+  test("initial clear", async () => {
+    await store.clear();
+    const len = await store.length();
+    assert.strictEqual(len, 0);
+  });
 
-      store.length((err, len) => {
-        t.error(err, "no error after clear");
-        t.equal(len, 0, "empty after clear");
-      });
+  test("set then clear", async () => {
+    await store.set("1092348234", {
+      cookie: {
+        maxAge: 1000,
+      },
+      name: "InsertThenClear",
+    });
+
+    const cleared = await store.clear();
+
+    assert.strictEqual(cleared, 1);
+
+    const len = await store.length();
+    assert.strictEqual(len, 0);
+  });
+
+  test("double clear", async () => {
+    await store.clear();
+    await store.clear();
+    const cleared = await store.clear();
+    assert.strictEqual(cleared, 0);
+
+    const len = await store.length();
+    assert.strictEqual(len, 0);
+  });
+
+  test("destroy", async () => {
+    await store.set("555666777", {
+      cookie: {
+        maxAge: 1000,
+      },
+      name: "Rob Dobilina",
+    });
+
+    await store.destroy("555666777");
+
+    const len = await store.length();
+    assert.strictEqual(len, 0);
+  });
+
+  test("set", async () => {
+    await store.set("1111222233334444", {
+      cookie: {
+        maxAge: 20000,
+      },
+      name: "sample name",
+    });
+
+    const len = await store.length();
+    assert.strictEqual(len, 1);
+  });
+
+  test("retrieve", async () => {
+    const session = await store.get("1111222233334444");
+
+    assert.deepEqual(session, {
+      cookie: {
+        maxAge: 20000,
+      },
+      name: "sample name",
     });
   });
 
-  test("set then clear", (t) => {
-    t.plan(4);
-
-    store
-      .set("1092348234", {
-        cookie: {
-          maxAge: 1000,
-        },
-        name: "InsertThenClear",
-      })
-      .then(() => {
-        store.clear((err, cleared) => {
-          t.error(err);
-          t.equal(1, cleared, "cleared 1");
-
-          store.length((err, len) => {
-            t.error(err, "no error after clear");
-            t.equal(len, 0, "empty after clear");
-          });
-        });
-      });
+  test("unknown session", async () => {
+    const item = await store.get("hope-and-change");
+    assert.equal(item, undefined);
   });
 
-  test("double clear", (t) => {
-    t.plan(4);
-
-    store
-      .clear()
-      .then(() => store.clear())
-      .then(() => {
-        store.clear((err, cleared) => {
-          t.error(err);
-          t.equal(0, cleared, "cleared 0");
-
-          store.length((err, len) => {
-            t.notOk(err, "no error after clear");
-            t.equal(len, 0, "length");
-          });
-        });
-      });
+  test("only one session should exist", async () => {
+    const len = await store.length();
+    assert.strictEqual(len, 1);
   });
 
-  test("destroy", (t) => {
-    t.plan(4);
+  test("touch", async () => {
+    await store.clear();
 
-    store.set(
-      "555666777",
-      {
-        cookie: {
-          maxAge: 1000,
-        },
-        name: "Rob Dobilina",
+    await store.set("11112222333344445555", {
+      cookie: {
+        maxAge: 20000,
       },
-      (err, rows) => {
-        t.error(err);
-        if (rows.rowCount && rows.rowCount > 1) {
-          t.fail("Row count too large");
-        }
-
-        store.destroy("555666777", (err) => {
-          t.error(err);
-
-          store.length((err, len) => {
-            t.error(err, "error");
-            t.equal(len, 0);
-          });
-        });
-      },
-    );
-  });
-
-  test("set", (t) => {
-    store.set(
-      "1111222233334444",
-      {
-        cookie: {
-          maxAge: 20000,
-        },
-        name: "sample name",
-      },
-      (err, rows) => {
-        t.error(err);
-        if (rows.rowCount) {
-          t.equal(rows.rowCount, 1, "row count");
-        }
-        t.end();
-      },
-    );
-  });
-
-  test("retrieve", (t) => {
-    t.plan(3);
-
-    store.get("1111222233334444", (err, session) => {
-      t.error(err);
-      t.ok(session, "session");
-      t.deepEqual(session, {
-        cookie: {
-          maxAge: 20000,
-        },
-        name: "sample name",
-      });
+      name: "sample name",
     });
-  });
-
-  test("unknown session", (t) => {
-    t.plan(2);
-
-    store.get("hope-and-change", (err, rows) => {
-      t.error(err);
-      t.equal(rows, undefined, "unknown session is not undefined");
+    const item = await store.touch("11112222333344445555", {
+      cookie: {
+        maxAge: 20000,
+        expires: new Date(),
+      },
+      name: "sample name",
     });
+    const len = await store.length();
+    assert.strictEqual(len, 1);
   });
 
-  test("only one session should exist", (t) => {
-    t.plan(2);
-
-    store.length((err, len) => {
-      t.error(err);
-      t.equal(len, 1);
-    });
-  });
-
-  test("touch", (t) => {
-    t.plan(3);
-
-    store
-      .clear()
-      .then(() =>
-        store.set("11112222333344445555", {
-          cookie: {
-            maxAge: 20000,
-          },
-          name: "sample name",
-        }),
-      )
-      .then(() => {
-        store.touch(
-          "11112222333344445555",
-          {
-            cookie: {
-              maxAge: 20000,
-              expires: new Date(),
-            },
-            name: "sample name",
-          },
-          (err) => {
-            t.error(err);
-
-            store.length((err, len) => {
-              t.error(err, "error");
-              t.equal(len, 1);
-            });
-          },
-        );
-      });
-  });
-
-  test("retrieve all", (t) => {
-    t.plan(6);
-    t.timeoutAfter(2000);
-
+  test("retrieve all", async () => {
     const session1 = {
       cookie: {
         maxAge: 20000,
@@ -235,34 +163,32 @@ stores.forEach((store) => {
       name: "retrieve-all session 2",
     };
 
-    return store
-      .clear()
-      .then(() => store.set("123412341234", session1, (err) => t.error(err)))
-      .then(() => store.set("432143214321", session2, (err) => t.error(err)))
-      .then(() =>
-        store.all((err, sessions) => {
-          t.error(err);
-          t.equal(sessions.length, 2);
-          sessions.forEach((session) => {
-            session.cookie.expires = new Date(session.cookie.expires);
-          });
-          t.deepLooseEqual(
-            sessions.find((s) => s.name === session1.name),
-            session1,
-          );
-          t.deepLooseEqual(
-            sessions.find((s) => s.name === session2.name),
-            session2,
-          );
-        }),
-      );
+    await store.clear();
+    await store.set("123412341234", session1);
+    await store.set("432143214321", session2);
+    const sessions = await store.all();
+    assert.equal(sessions.length, 2);
+
+    sessions.forEach((session) => {
+      session.cookie.expires = new Date(session.cookie.expires);
+    });
+
+    assert.deepEqual(
+      sessions.find((s) => s.name === session1.name),
+      session1,
+    );
+
+    assert.deepEqual(
+      sessions.find((s) => s.name === session2.name),
+      session2,
+    );
   });
 
-  test("no cleanup timeout when disableDbCleanup is true", (t) => {
-    t.equal(store.getNextDbCleanup(), null);
-    t.end();
+  test("no cleanup timeout when disableDbCleanup is true", () => {
+    assert.equal(store.getNextDbCleanup(), null);
   });
-  test("cleanup", (t) => {
-    store.knex.destroy().then(t.end);
+
+  test("cleanup", async () => {
+    await store.knex.destroy();
   });
 });
