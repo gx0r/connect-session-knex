df0a4d0bd3fb1a00ce49416d7408e98ca38a0dda
Grant Miner <gx0r@proton.me>
2024-06-05 20:25:36 -0500

    node globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
 ];
