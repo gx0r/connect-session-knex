a25ae703bf9f7f8de494918efc4534f7a9ac2604
Grant Miner <gx0r@proton.me>
2024-06-09 19:48:27 -0500

    ts eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,27 @@
+import { ESLint } from "eslint";
+import parser from "@typescript-eslint/parser";
+import plugin from "@typescript-eslint/eslint-plugin";
+
 import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
-  eslintConfigPrettier,
-];
+/** @type {ESLint.ConfigData} */
+export default {
+  files: ["src/**/*.ts", "test/**/*.mjs"],
+  languageOptions: {
+    parser,
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      project: "./tsconfig.json",
+    },
+    globals: globals.node,
+  },
+  plugins: {
+    "@typescript-eslint": plugin,
+  },
+  rules: {
+    ...eslintConfigPrettier.rules,
+  },
+  ...eslintConfigPrettier.configs,
+};
